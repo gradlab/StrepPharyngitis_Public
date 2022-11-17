76a7bbb345f6cbd2c43af5be1a4f55f51035bdab
Stephen Kissler <skissler@hsph.harvard.edu>
2022-11-17 11:30:43 -0500

    update

--- reduce_visits.R
+++ reduce_visits.R
@@ -1,22 +1,47 @@
 library(tidyverse) 
 
-GeoCohort10 <- read_csv("output/GeoCohort10.csv")
-GeoCohort11 <- read_csv("output/GeoCohort11.csv")
-GeoCohort12 <- read_csv("output/GeoCohort12.csv")
-GeoCohort13 <- read_csv("output/GeoCohort13.csv")
-GeoCohort14 <- read_csv("output/GeoCohort14.csv")
-GeoCohort15 <- read_csv("output/GeoCohort15.csv")
-GeoCohort16 <- read_csv("output/GeoCohort16.csv")
-GeoCohort17 <- read_csv("output/GeoCohort17.csv")
-GeoCohort18 <- read_csv("output/GeoCohort18.csv")
+GeoCohort10 <- read_csv("output/private/GeoCohort10_2022-11-16.csv")
+GeoCohort11 <- read_csv("output/private/GeoCohort11_2022-11-16.csv")
+GeoCohort12 <- read_csv("output/private/GeoCohort12_2022-11-16.csv")
+GeoCohort13 <- read_csv("output/private/GeoCohort13_2022-11-16.csv")
+GeoCohort14 <- read_csv("output/private/GeoCohort14_2022-11-16.csv")
+GeoCohort15 <- read_csv("output/private/GeoCohort15_2022-11-16.csv")
+GeoCohort16 <- read_csv("output/private/GeoCohort16_2022-11-16.csv")
+GeoCohort17 <- read_csv("output/private/GeoCohort17_2022-11-16.csv")
+GeoCohort18 <- read_csv("output/private/GeoCohort18_2022-11-16.csv")
 
-GeoVisits10 <- read_csv("output/GeoVisits10.csv")
-GeoVisits11 <- read_csv("output/GeoVisits11.csv")
-GeoVisits12 <- read_csv("output/GeoVisits12.csv")
-GeoVisits13 <- read_csv("output/GeoVisits13.csv")
-GeoVisits14 <- read_csv("output/GeoVisits14.csv")
-GeoVisits15 <- read_csv("output/GeoVisits15.csv")
-GeoVisits16 <- read_csv("output/GeoVisits16.csv")
-GeoVisits17 <- read_csv("output/GeoVisits17.csv")
-GeoVisits18 <- read_csv("output/GeoVisits18.csv")
+GeoVisits10 <- read_csv("output/private/GeoVisits10_2022-11-16.csv")
+GeoVisits11 <- read_csv("output/private/GeoVisits11_2022-11-16.csv")
+GeoVisits12 <- read_csv("output/private/GeoVisits12_2022-11-16.csv")
+GeoVisits13 <- read_csv("output/private/GeoVisits13_2022-11-16.csv")
+GeoVisits14 <- read_csv("output/private/GeoVisits14_2022-11-16.csv")
+GeoVisits15 <- read_csv("output/private/GeoVisits15_2022-11-16.csv")
+GeoVisits16 <- read_csv("output/private/GeoVisits16_2022-11-16.csv")
+GeoVisits17 <- read_csv("output/private/GeoVisits17_2022-11-16.csv")
+GeoVisits18 <- read_csv("output/private/GeoVisits18_2022-11-16.csv")
 
+GeoCohort <- bind_rows(
+	mutate(GeoCohort10,YEAR="2010"),
+	mutate(GeoCohort11,YEAR="2011"),
+	mutate(GeoCohort12,YEAR="2012"),
+	mutate(GeoCohort13,YEAR="2013"),
+	mutate(GeoCohort14,YEAR="2014"),
+	mutate(GeoCohort15,YEAR="2015"),
+	mutate(GeoCohort16,YEAR="2016"),
+	mutate(GeoCohort17,YEAR="2017"),
+	mutate(GeoCohort18,YEAR="2018"))
+
+GeoVisits <- bind_rows(
+	mutate(GeoVisits10,YEAR="2010"),
+	mutate(GeoVisits11,YEAR="2011"),
+	mutate(GeoVisits12,YEAR="2012"),
+	mutate(GeoVisits13,YEAR="2013"),
+	mutate(GeoVisits14,YEAR="2014"),
+	mutate(GeoVisits15,YEAR="2015"),
+	mutate(GeoVisits16,YEAR="2016"),
+	mutate(GeoVisits17,YEAR="2017"),
+	mutate(GeoVisits18,YEAR="2018")) %>% 
+	filter(PRIMARYCOND=="Strep pharyngitis")
+
+write_csv(GeoCohort, file="output/GeoCohort.csv")
+write_csv(GeoVisits, file="output/GeoVisits.csv")
